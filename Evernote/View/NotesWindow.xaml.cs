913aa5df661108e661e0dc385f467d2db622a303
Bart Zawada <b.zawada84@gmail.com>
2023-12-21 13:53:25 +0100

    Design view for NotesWindow
    Add bindings to data and commands in NotesWindow.

---
+++ Evernote/View/NotesWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Evernote.View
+{
+    /// <summary>
+    /// Interaction logic for NoteWindow.xaml
+    /// </summary>
+    public partial class NotesWindow : Window
+    {
+        public NotesWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void SpeachButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
