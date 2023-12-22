4e849025e92154b9d8efcd9442f98bfcb205f528
Bart Zawada <b.zawada84@gmail.com>
2023-12-22 09:26:56 +0100

    Add document field for filling notes content.
    1. Add speach recognition.
    2. Add document status bar with number of characters.

--- Evernote/View/NotesWindow.xaml.cs
+++ Evernote/View/NotesWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using Evernote.ViewModel.Helpers;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.Windows;
+using System.Windows.Documents;
 
 namespace Evernote.View
 {
@@ -17,9 +21,21 @@ namespace Evernote.View
             Application.Current.Shutdown();
         }
 
-        private void SpeachButton_Click(object sender, RoutedEventArgs e)
+        private async void SpeachButton_Click(object sender, RoutedEventArgs e)
         {
+            var recordedText = await SpeechToTextRecorder.Record();
+            contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(recordedText)));
+        }
 
+        private void contentRichTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            var totalCharacters = new TextRange(contentRichTextBox.Document.ContentStart, contentRichTextBox.Document.ContentEnd).Text.Trim().Length;
+            statusTextBlock.Text = totalCharacters > 0 ? $"Document length: {totalCharacters} characters" : "";
+        }
+
+        private void boldButton_Click(object sender, RoutedEventArgs e)
+        {
+            contentRichTextBox.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
         }
     }
 }
---
+++ Evernote/ViewModel/Helpers/SpeachToTextRecorder.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+namespace Evernote.ViewModel.Helpers
+{
+    public static class SpeechToTextRecorder
+    {
+        private const string Region = "germanywestcentral";
+        private const string Key = "986a546b737b4964a698418afe101b30";
+
+        public static async Task<string> Record()
+        {
+            var speechConfig = SpeechConfig.FromSubscription(Region, Key);
+            using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+            var result = await recognizer.RecognizeOnceAsync();
+
+            return result.Text;
+        }
+    }
+}
