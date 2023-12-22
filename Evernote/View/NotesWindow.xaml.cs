976bb4833f91b53dc57023ad600dabdbbd18a4ab
Bart Zawada <b.zawada84@gmail.com>
2023-12-22 14:15:08 +0100

    Add text formatting for the document.
    1. Add bold, italic and underline toggle buttons.
    2. Add font family and font size combo boxes.

--- Evernote/View/NotesWindow.xaml.cs
+++ Evernote/View/NotesWindow.xaml.cs
@@ -2,7 +2,9 @@
 using Microsoft.CognitiveServices.Speech;
 using Microsoft.CognitiveServices.Speech.Audio;
 using System.Windows;
+using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace Evernote.View
 {
@@ -14,6 +16,8 @@ namespace Evernote.View
         public NotesWindow()
         {
             InitializeComponent();
+            fontFamilyComboBox.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            fontSizeComboBox.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 32, 48, 72 };
         }
 
         private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
@@ -23,19 +27,123 @@ namespace Evernote.View
 
         private async void SpeachButton_Click(object sender, RoutedEventArgs e)
         {
-            var recordedText = await SpeechToTextRecorder.Record();
-            contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(recordedText)));
+            try
+            {
+                var recordedText = await SpeechToTextRecorder.Record();
+                contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(recordedText)));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error starting speech recognition\nException message: {ex.Message}", "Operation failed", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
-        private void contentRichTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        private void ContentRichTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
             var totalCharacters = new TextRange(contentRichTextBox.Document.ContentStart, contentRichTextBox.Document.ContentEnd).Text.Trim().Length;
             statusTextBlock.Text = totalCharacters > 0 ? $"Document length: {totalCharacters} characters" : "";
         }
 
-        private void boldButton_Click(object sender, RoutedEventArgs e)
+        private void ContentRichTextBox_SelectionChanged(object sender, RoutedEventArgs e)
         {
-            contentRichTextBox.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+            ToggleBoldButtonState();
+            ToggleItalicButtonState();
+            ToggleUnderlineButtonState();
+            fontFamilyComboBox.SelectedItem = contentRichTextBox.Selection.GetPropertyValue(FontFamilyProperty);
+            fontSizeComboBox.Text = contentRichTextBox.Selection.GetPropertyValue(FontSizeProperty).ToString();
+        }
+
+        private void ToggleUnderlineButtonState()
+        {
+            var textDecorations = contentRichTextBox.Selection.GetPropertyValue(Inline.TextDecorationsProperty) as TextDecorationCollection;
+            if (textDecorations != null) 
+            {
+                foreach (var textDecoration in textDecorations)
+                {
+                    if (textDecoration.Location == TextDecorationLocation.Underline)
+                    {
+                        underlineButton.IsChecked = true;
+                        return;
+                    }
+                }
+            }
+
+            underlineButton.IsChecked = false;
+        }
+
+        private void ToggleItalicButtonState()
+        {
+            var selectedStyle = contentRichTextBox.Selection.GetPropertyValue(FontStyleProperty);
+            italicButton.IsChecked = selectedStyle != null &&
+                selectedStyle.ToString() == FontStyles.Italic.ToString();
+        }
+
+        private void ToggleBoldButtonState()
+        {
+            var selectedWeight = contentRichTextBox.Selection.GetPropertyValue(FontWeightProperty);
+            boldButton.IsChecked = selectedWeight != null &&
+                selectedWeight.ToString() == FontWeights.Bold.ToString();
+        }
+
+        private void BoldButton_Click(object sender, RoutedEventArgs e)
+        {
+            var isChecked = (sender as ToggleButton).IsChecked ?? false;
+            if (isChecked)
+            {
+                contentRichTextBox.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
+            }
+            else
+            {
+                contentRichTextBox.Selection.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Normal);
+            }
+        }
+
+        private void ItalicButton_Click(object sender, RoutedEventArgs e)
+        {
+            var isChecked = (sender as ToggleButton).IsChecked ?? false;
+            if (isChecked)
+            {
+                contentRichTextBox.Selection.ApplyPropertyValue(FontStyleProperty, FontStyles.Italic);
+            }
+            else
+            {
+                contentRichTextBox.Selection.ApplyPropertyValue(FontStyleProperty, FontStyles.Normal);
+            }
+        }
+
+        private void UnderlineButton_Click(object sender, RoutedEventArgs e)
+        {
+            var isChecked = (sender as ToggleButton).IsChecked ?? false;
+            if (isChecked)
+            {
+                contentRichTextBox.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
+            }
+            else
+            {
+                (contentRichTextBox.Selection.GetPropertyValue(Inline.TextDecorationsProperty) as TextDecorationCollection)
+                    .TryRemove(TextDecorations.Underline, out var textDecorations);
+                contentRichTextBox.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, textDecorations);
+            }
+        }
+
+        private void FontFamilyComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (fontFamilyComboBox.SelectedItem is null) 
+            {
+                return;
+            }
+
+            contentRichTextBox.Selection.ApplyPropertyValue(FontFamilyProperty, fontFamilyComboBox.SelectedItem);
+        }
+
+        private void FontSizeComboBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (fontSizeComboBox.SelectedItem is null) 
+            {
+                return;
+            }
+
+            contentRichTextBox.Selection.ApplyPropertyValue(FontSizeProperty, fontSizeComboBox.SelectedItem);
         }
     }
 }
